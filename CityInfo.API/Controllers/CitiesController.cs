d396527a205ff903e0c72840a2abea67fdebad2a
Oksimba <oksana.strus@eleks.com>
2023-04-21 15:14:25 +0300

    Inject CitiesDataStore

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -7,16 +7,23 @@ namespace CityInfo.API.Controllers
     [Route("api/[controller]")]
     public class CitiesController : ControllerBase
     {
+        private readonly CitiesDataStore _citiesDataStore;
+
+        public CitiesController(CitiesDataStore citiesDataStore)
+        {
+            _citiesDataStore = citiesDataStore ?? throw new ArgumentException(nameof(citiesDataStore)); ;
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<CityDto>> GetCities()
         {
-            return Ok(CitiesDataStore.Current.Cities);
+            return Ok(_citiesDataStore.Cities);
         }
 
         [HttpGet("{id}")]
         public ActionResult<CityDto> GetCity(int id)
         {
-            var result = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            var result = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
 
             if(result ==null)
             {
