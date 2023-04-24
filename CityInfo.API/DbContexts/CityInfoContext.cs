0059d1d15039aff17a6970cb137cc6b9dbeb1b08
Oksimba <oksana.strus@eleks.com>
2023-04-24 14:39:10 +0300

    Add dbContext, add migrations

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -5,7 +5,19 @@ namespace CityInfo.API.DbContexts
 {
     public class CityInfoContext: DbContext
     {
-        public DbSet<City> Cities { get; set; }
-        public DbSet<PointOfInterest> PointsOfInterests { get; set;}
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointsOfInterests { get; set;} = null!;
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) 
+            : base(options) 
+        {
+
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
     }
 }
